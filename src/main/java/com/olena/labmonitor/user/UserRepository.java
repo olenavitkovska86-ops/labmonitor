c051cc639b2a2a4f46031d062a2e5589b800e595
Dhannea <dhanneatapic@gmail.com>
2026-08-11 15:38:45 +0200

    unecessary shi

--- src/main/java/com/olena/labmonitor/user/UserRepository.java
+++ src/main/java/com/olena/labmonitor/user/UserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.lang.ScopedValue;
+//import java.lang.ScopedValue;
 import java.util.List;
 import java.util.Optional;
 
